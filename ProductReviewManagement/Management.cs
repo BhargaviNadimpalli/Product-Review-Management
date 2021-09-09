3f4cb8409e0287e8f34cf47d7e51f7d8e1c67938
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-09 17:14:15 -0600

    Feat: Retrieving records from list

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -22,5 +22,18 @@ namespace ProductReviewManagement
             }
 
         }
+        public void SelectRecords(List<ProductReview> productReviews)
+        {
+            var recordedData = from ProductReview in productReviews
+                               where (ProductReview.ProductId == 1 || ProductReview.ProductId == 4 || ProductReview.ProductId == 9)
+                               && ProductReview.Rating > 3
+                               select ProductReview;
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId : " + list.ProductId + " " + "UserId : " + " " + list.UserId
+                   + " " + "Rating : " + list.Rating + " " + "Review : " + list.Review + " " + "IsLike : " + list.IsLike);
+            }
+        }
     }
 }
