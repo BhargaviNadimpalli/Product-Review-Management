d21bbae363dd66a0e0c8efb4f628ffbbad1d906b
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-09 17:10:37 -0600

    Feat: Top 3 records from rating

---
+++ ProductReviewManagement/Management.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public readonly DateTime dateTime = new DateTime();
+
+        public void TopRecord(List<ProductReview> productReviews)
+        {
+            var recordedData = (from products in productReviews
+                                orderby products.Rating descending
+                                select products).Take(3);
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId : " + list.ProductId + " " + "UserId : " + " " + list.UserId
+                   + " " + "Rating : " + list.Rating + " " + "Review : " + list.Review + " " + "IsLike : " + list.IsLike);
+            }
+
+        }
+    }
+}
